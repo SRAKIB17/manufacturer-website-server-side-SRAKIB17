2937b79c2da6958c0eb998b6fa52f640bf2d3a38
SRAKIB17 <rakibulssc5@gmail.com>
2022-05-26 22:50:15 +0600

    'done'

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ const run = async () => {
             res.send(result)
         }
     })
-
+     
     app.delete('/user/:id', verifyJWT, verifyAdmin, async (req, res) => {
         const id = req.params.id;
         const query = { _id: ObjectId(id) }
